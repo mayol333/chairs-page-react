4b9d491a89c8455377bb21fc71ad4d1aabfcbe3e
mayol333 <s.moniczewski@gmail.com>
2023-10-20 19:45:01 +0200

    end chair

--- src/components/FiveSection.js
+++ src/components/FiveSection.js
@@ -1,33 +1,81 @@
+import { useState } from "react";
 export const FiveSection = () => {
+    const [contact, setContact] = useState({
+        name: "",
+        email: "",
+        message: "",
+        checkbox: false,
+    });
+    const handleNameChange = (event) => {
+        const { value } = event.target;
+        setContact({ ...contact, name: value });
+    };
+    const handleEmailChange = (event) => {
+        const { value } = event.target;
+        setContact({ ...contact, email: value });
+    };
+    const handleMessageChange = (event) => {
+        const { value } = event.target;
+        setContact({ ...contact, message: value });
+    };
+    const handleCheckboxChange = () => {
+        const value = !contact.checkbox;
+        setContact({ ...contact, checkbox: value });
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(contact);
+    };
     return (
         <section className="five-section">
-            <div className="width-five-section">
-                <div className="kontakt">
-                    <p className="contact">kontakt</p>
-                    <input className="name" type="text" placeholder="Imię" />
-                    <input className="email" type="text" placeholder="email" />
-                    <textarea
-                        className="message"
-                        name=""
-                        id=""
-                        cols="30"
-                        rows="10"
-                        placeholder="Wiadomość"
-                    ></textarea>
+            <form onSubmit={handleSubmit}>
+                <div className="width-five-section">
+                    <div className="kontakt">
+                        <p className="contact">kontakt</p>
+                        <input
+                            onChange={handleNameChange}
+                            className="name"
+                            type="text"
+                            placeholder="Imię"
+                            value={contact.name}
+                        />
+                        <input
+                            onChange={handleEmailChange}
+                            className="email"
+                            type="text"
+                            placeholder="email"
+                            value={contact.email}
+                        />
+                        <textarea
+                            onChange={handleMessageChange}
+                            className="message"
+                            name=""
+                            id=""
+                            cols="30"
+                            rows="10"
+                            placeholder="Wiadomość"
+                            value={contact.message}
+                        ></textarea>
+                    </div>
+                    <div className="send">
+                        <img
+                            alt=""
+                            className="last-photo"
+                            src="/public/images/red_chair.png"
+                        />
+                        <label className="label" for="">
+                            <input
+                                className="confirm"
+                                type="checkbox"
+                                onChange={handleCheckboxChange}
+                                value={contact.checkbox}
+                            />
+                            Zgadzam się na przetwarzanie danych osobowych
+                        </label>
+                        <button className="last-click active">wyślij</button>
+                    </div>
                 </div>
-                <div className="send">
-                    <img
-                        alt=""
-                        className="last-photo"
-                        src="/public/images/red_chair.png"
-                    />
-                    <label className="label" for="">
-                        <input className="confirm" type="checkbox" />
-                        Zgadzam się na przetwarzanie danych osobowych
-                    </label>
-                    <button className="last-click active">wyślij</button>
-                </div>
-            </div>
+            </form>
         </section>
     );
 };
